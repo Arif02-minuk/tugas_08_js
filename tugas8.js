75487bae34f1bef04f09efa74a2879eb355ed139
arif wijanarko <arifpethod@gmail.com>
2023-06-16 08:59:53 +0700

    tugas js 08
    tugas untuk niomic

---
+++ tugas8.js
@@ -0,0 +1,12 @@
+function barang() {
+    var laptop = {
+        merk: "asus",
+        warna: "hitam",
+    }
+    console.log(laptop);
+    laptop.harga = 5000000;
+    laptop.detail = ["ram 12gb", "core i7", "rom 256gb"]
+    console.log(laptop)
+}
+
+barang();
